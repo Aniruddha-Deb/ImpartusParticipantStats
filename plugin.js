b9bb52eaf4e1f45a8e1ea526df3a76727dba9588
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2021-03-09 16:09:24 +0530

    Fixed loading bug, works like a charm now

--- plugin.js
+++ plugin.js
@@ -16,20 +16,3 @@ if (window.location.href.startsWith("https://a.impartus.com/vc/#/ttid")) {
 		observer.observe(attendees, config);
 	}, false);
 }	
-/*
-// Chrome content script matcher does not seem to play well with # in URL's, hmm..
-if (window.location.href.startsWith("https://a.impartus.com/vc/#/in/ttid")) {
-
-	var config = { attributes: true };
-	var attendees = $("div[imus-attendee-list]")[0];
-	var participants = $("div[class=i-vc-right-panel-tab-header]")[0].lastElementChild;
-	
-	var callback = function(mutationsList) {
-		var numParticipants = attendees.childElementCount - 2;
-		participants.innerText = "Participants(" + numParticipants + ")";
-		console.log('Updated participants');
-	};
-	
-	var observer = new MutationObserver(callback);
-	observer.observe(attendees, config);
-}*/
